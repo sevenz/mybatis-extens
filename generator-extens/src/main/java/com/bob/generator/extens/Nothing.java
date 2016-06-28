efbd597f289cae208bb7ff5d99eeb3751cb65c2d
王相 <wangxiang@51zhangdan.com.cn>
2016-06-28 17:34:01 +0800

    添加建表语句

--- generator-extens/src/main/java/com/bob/generator/extens/Nothing.java
+++ generator-extens/src/main/java/com/bob/generator/extens/Nothing.java
@@ -2,4 +2,13 @@ package com.bob.generator.extens;
 
 public class Nothing {
 
+    /**
+     *
+     CREATE TABLE `mybatis_bb_00` (
+     `autoId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键自增长',
+     `name` varchar(5) COLLATE utf8_bin NOT NULL COMMENT '姓名',
+     `userId` bigint(20) NOT NULL COMMENT '用户分表标识',
+     PRIMARY KEY (`autoId`)
+     ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户标识测试';
+     */
 }
