b7de7006b5bd8d16cad74b8a5cabe293237559bb
wangxiang <fuck51test>
2017-06-26 18:04:52 +0800

    添加是不是需要生成selectOneByExample的判断

--- generator-extens/src/main/java/com/bob/generator/extens/plugins/SelectOneByExamplePlugin.java
+++ generator-extens/src/main/java/com/bob/generator/extens/plugins/SelectOneByExamplePlugin.java
@@ -30,7 +30,10 @@ public class SelectOneByExamplePlugin extends PluginAdapterEnhancement {
     @Override
     public boolean clientSelectByExampleWithBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
         if (!config.shouldExclude(interfaze.getType())) {
-            interfaze.addMethod(generateSelectOneByExample(method, introspectedTable));
+            Method temp = generateSelectOneByExample(method, introspectedTable);
+            if(temp != null) {
+                interfaze.addMethod(temp);
+            }
         }
         return super.clientSelectByExampleWithBLOBsMethodGenerated(method, interfaze, introspectedTable);
     }
@@ -38,10 +41,36 @@ public class SelectOneByExamplePlugin extends PluginAdapterEnhancement {
     @Override
     public boolean clientSelectByExampleWithBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
         if (!config.shouldExclude(topLevelClass.getType())) {
-            topLevelClass.addMethod(generateSelectOneByExample(method, introspectedTable));
+            Method temp = generateSelectOneByExample(method, introspectedTable);
+            if(temp != null) {
+                topLevelClass.addMethod(temp);
+            }
         }
         return super.clientSelectByExampleWithBLOBsMethodGenerated(method, topLevelClass, introspectedTable);
     }
+
+    @Override
+    public boolean clientSelectByExampleWithoutBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
+        if (!config.shouldExclude(interfaze.getType())) {
+            Method temp = generateSelectOneByExample(method, introspectedTable);
+            if(temp != null) {
+                interfaze.addMethod(temp);
+            }
+        }
+        return super.clientSelectByExampleWithoutBLOBsMethodGenerated(method, interfaze, introspectedTable);
+    }
+
+    @Override
+    public boolean clientSelectByExampleWithoutBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        if (!config.shouldExclude(topLevelClass.getType())) {
+            Method temp = generateSelectOneByExample(method, introspectedTable);
+            if(temp != null) {
+                topLevelClass.addMethod(temp);
+            }
+        }
+        return super.clientSelectByExampleWithoutBLOBsMethodGenerated(method, topLevelClass, introspectedTable);
+    }
+
 
     @Override
     public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
@@ -78,22 +107,6 @@ public class SelectOneByExamplePlugin extends PluginAdapterEnhancement {
         return super.sqlMapDocumentGenerated(document, introspectedTable);
     }
 
-    @Override
-    public boolean clientSelectByExampleWithoutBLOBsMethodGenerated(Method method, Interface interfaze, IntrospectedTable introspectedTable) {
-        if (!config.shouldExclude(interfaze.getType())) {
-            interfaze.addMethod(generateSelectOneByExample(method, introspectedTable));
-        }
-        return super.clientSelectByExampleWithoutBLOBsMethodGenerated(method, interfaze, introspectedTable);
-    }
-
-    @Override
-    public boolean clientSelectByExampleWithoutBLOBsMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
-        if (!config.shouldExclude(topLevelClass.getType())) {
-            topLevelClass.addMethod(generateSelectOneByExample(method, introspectedTable));
-        }
-        return super.clientSelectByExampleWithoutBLOBsMethodGenerated(method, topLevelClass, introspectedTable);
-    }
-
     /**
      * 在XXXMapper中添加一个selectOnyByExample方法
      *
@@ -102,28 +115,32 @@ public class SelectOneByExamplePlugin extends PluginAdapterEnhancement {
      * @return
      */
     private Method generateSelectOneByExample(Method method, IntrospectedTable introspectedTable) {
-        should_generator_one_method = true;
-        Method m = new Method(config.methodToGenerate);
-        m.setVisibility(method.getVisibility());
-        FullyQualifiedJavaType returnType = introspectedTable.getRules().calculateAllFieldsClass();
-        m.setReturnType(returnType);
+        if (!should_generator_one_method) {
+            should_generator_one_method = true;
+            Method m = new Method(config.methodToGenerate);
+            m.setVisibility(method.getVisibility());
+            FullyQualifiedJavaType returnType = introspectedTable.getRules().calculateAllFieldsClass();
+            m.setReturnType(returnType);
 
-        List<String> annotations = method.getAnnotations();
-        for (String a : annotations) {
-            m.addAnnotation(a);
+            List<String> annotations = method.getAnnotations();
+            for (String a : annotations) {
+                m.addAnnotation(a);
+            }
+
+            List<Parameter> params = method.getParameters();
+            for (Parameter p : params) {
+                m.addParameter(p);
+            }
+
+            m.addJavaDocLine("/**");
+            m.addJavaDocLine(" * 根据查询条件返回一条，找不到则返回null，找到多余一条数据则拋错");
+            m.addJavaDocLine(" */");
+            m.addException(new FullyQualifiedJavaType("org.apache.ibatis.exceptions.TooManyResultsException"));
+            context.getCommentGenerator().addGeneralMethodComment(m, introspectedTable);
+            return m;
+        } else {
+            return null;
         }
-
-        List<Parameter> params = method.getParameters();
-        for (Parameter p : params) {
-            m.addParameter(p);
-        }
-
-        m.addJavaDocLine("/**");
-        m.addJavaDocLine(" * 根据查询条件返回一条，找不到则返回null，找到多余一条数据则拋错");
-        m.addJavaDocLine(" */");
-        m.addException( new FullyQualifiedJavaType("org.apache.ibatis.exceptions.TooManyResultsException"));
-        context.getCommentGenerator().addGeneralMethodComment(m, introspectedTable);
-        return m;
     }
 
     private static final class Config extends PluginConfig {
